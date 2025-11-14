23ce2a06533d346645abaacb3ff8bb23dcea522f
Ray Ogoda <ray.ogoda@evolany.com>
2025-11-14 09:40:21 +0900

    Add some of the exercises from lecture 18

---
+++ 18_memory/uba.h
@@ -0,0 +1,41 @@
+#ifndef UBA_HEADER
+#define UBA_HEADER
+
+typedef struct uba_header uba;
+
+// Client type
+typedef uba* uba_t;
+
+// typedef ______* uba_t;
+int uba_len(uba_t A)
+/*@requires A != NULL; @*/
+/*@ensures \result >= 0; @*/ ;
+
+uba_t uba_new(int size)
+/*@requires 0 <= size; @*/
+/*@ensures \result != NULL; @*/
+/*@ensures uba_len(\result) == size; @*/ ;
+
+char uba_get(uba_t A, int i)
+/*@requires A != NULL; @*/
+/*@requires 0 <= i && i < uba_len(A); @*/ ;
+
+void uba_set(uba_t A, int i, char x)
+/*@requires A != NULL; @*/
+/*@requires 0 <= i && i < uba_len(A); @*/ ;
+
+void uba_add(uba_t A, char x)
+/*@requires A != NULL; @*/ ;
+
+char uba_rem(uba_t A)
+/*@requires A != NULL; @*/
+/*@requires 0 < uba_len(A); @*/ ;
+
+// bonus function
+void uba_print(uba_t A)
+/*@requires A != NULL; @*/ ;
+
+void uba_free(uba_t A)
+//@requires A != NULL;
+;
+#endif

4b00524c059f130101167a5f0649f3765579d2f6
Ray Ogoda <ray.ogoda@evolany.com>
2025-11-17 16:58:30 +0900

    Add lecture 18

--- 18-memory/uba.h
+++ 18-memory/uba.h
@@ -1,33 +1,32 @@
-#ifndef UBA_HEADER
-#define UBA_HEADER
+#ifndef UBA_H
+#define UBA_H
 
-typedef struct uba_header uba;
+#include <stddef.h>
+typedef struct uba_header* uba_t;
 
-// Client type
-typedef uba* uba_t;
 
 // typedef ______* uba_t;
-int uba_len(uba_t A)
+size_t uba_len(uba_t A)
 /*@requires A != NULL; @*/
 /*@ensures \result >= 0; @*/ ;
 
-uba_t uba_new(int size)
+uba_t uba_new(size_t size)
 /*@requires 0 <= size; @*/
 /*@ensures \result != NULL; @*/
 /*@ensures uba_len(\result) == size; @*/ ;
 
-char uba_get(uba_t A, int i)
+char* uba_get(uba_t A, size_t i)
 /*@requires A != NULL; @*/
 /*@requires 0 <= i && i < uba_len(A); @*/ ;
 
-void uba_set(uba_t A, int i, char x)
+void uba_set(uba_t A, size_t i, char* x)
 /*@requires A != NULL; @*/
 /*@requires 0 <= i && i < uba_len(A); @*/ ;
 
-void uba_add(uba_t A, char x)
+void uba_add(uba_t A, char* x)
 /*@requires A != NULL; @*/ ;
 
-char uba_rem(uba_t A)
+char* uba_rem(uba_t A)
 /*@requires A != NULL; @*/
 /*@requires 0 < uba_len(A); @*/ ;
 
